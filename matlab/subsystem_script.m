64d0fafc3de001498bfa76fb1244ef4ec609b9c5
Nathan Lord <lord.59@osu.edu>
2014-11-01 17:45:33 -0400

    Documentation

--- matlab/subsystem_script.m
+++ matlab/subsystem_script.m
@@ -1,25 +1,35 @@
 clear all
+
+% get workspace of the external subsystem
 sub = get_param('test_create_subsystem','ModelWorkspace');
 
+% choose what parameter to fill
 x = 2;
+
+%load parameters. ALWAYS DO THIS BEFORE RUNNING A MODEL! YOU DON'T KNOW
+%WHAT VARIABLES ARE IN THE WORKSPACE FROM PREVIOUS RUNS 
 sub.reload
+
+%run simulation
 sim('use_test')
 out(1)
 % should be 2
 
-
+%change parameter A and run
 assignin(sub,'A',10)
 
 sim('use_test')
 out(1)
 % should be 10
 
+%etc
 x = 3;
 sub.reload
 sim('use_test')
 out(1)
 % should be 3
 
+%etc
 assignin(sub,'A',10)
 
 sim('use_test')
