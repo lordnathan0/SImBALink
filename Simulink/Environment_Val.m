ab423c1e2e46071eeb18ee56b37ac87322ccf11e
Nathan Lord <lord.59@osu.edu>
2014-11-11 22:54:01 -0500

    Environment import iom data

--- Simulink/Environment_Val.m
+++ Simulink/Environment_Val.m
@@ -1,4 +1,5 @@
 load Environment_Val.mat
+close all
 
 %must have data:
 %   P = preassure in Pa
@@ -17,3 +18,12 @@ UT = [t',t'];
 opt = simset('ReturnWorkspaceOutputs', 'off');
 [T,X,Y] = sim('Environment',Timespan,opt, UT);
 sub.reload;
+
+figure
+hold all
+plot(alt,rho)
+plot(Y(:,6),Y(:,4))
+legend('data','model')
+title('Validate Density')
+xlabel('Altitude')
+ylabel('Density')
