209c8d670582a83cb18da6ad2df3c4ec446a8f26
Nathan Lord <lord.59@osu.edu>
2014-11-13 17:19:25 -0500

    True gear validation

--- Simulink/Geal_val.m
+++ Simulink/Geal_val.m
@@ -3,9 +3,10 @@ sub = get_param('Gear_Chain','ModelWorkspace');
 sub.reload;
 
 %%
-
+close all
+Rg = evalin(sub,'Rg');
 dur = 100;
-Tm = ones(1,dur) * 10;
+Tm = ones(1,dur) * 10 * Rg;
 Tt = linspace(0,10,dur);
 t = [1:1:dur];
 Timespan = max(t);
@@ -14,3 +15,8 @@ UT = [t',Tm',Tt'];
 opt = simset('ReturnWorkspaceOutputs', 'off');
 [T,X,Y] = sim('Gear_Chain',Timespan,opt, UT);
 sub.reload;
+
+figure
+plot(Y(:,1));
+figure
+plot(Y(:,2));
