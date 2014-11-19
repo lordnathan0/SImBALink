16ee2b2019317bf305b0d8f1dd74395a4bcf66a5
Nathan Lord <lord.59@osu.edu>
2014-11-19 00:02:20 -0500

    trying to validate vehicle

---
+++ Simulink/Vehicle_Val.m
@@ -0,0 +1,32 @@
+close all
+
+v0 = 20;
+chas = get_param('Chassis','ModelWorkspace');
+gear = get_param('Gear_Chain','ModelWorkspace');
+tire = get_param('Tires','ModelWorkspace');
+
+assignin(chas,'v0',v0);
+g1 = getVariable(tire,'Ct');
+g2 = getVariable(gear,'Rg');
+
+
+
+rho = 1.2;
+Or = 0;
+Beta = 0;
+V_cmd = 30;
+Rc = 10000;
+ks_cmd = .15;
+
+assignin(gear,'w0',(ks_cmd*v0-v0)/-g1);
+
+
+
+dur = 75;
+sim('Vehicle_Validation', dur)
+
+figure
+plot(t,v)
+
+figure
+plot(t,ks)
