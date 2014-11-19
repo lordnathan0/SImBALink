fcec23f6bc30f0284d4911d5e8d90e463b3c1c91
Nathan Lord <lord.59@osu.edu>
2014-11-19 16:37:09 -0500

    Gear Validation Documentation

--- Simulink/Geal_val.m
+++ Simulink/Geal_val.m
@@ -6,7 +6,7 @@ sub.reload;
 close all
 Rg = evalin(sub,'Rg');
 dur = 100;
-Tm = ones(1,dur) * 10 * Rg;
+Tm = ones(1,dur) * 10;
 Tt = linspace(0,10,dur);
 t = [1:1:dur];
 Timespan = max(t);
@@ -17,12 +17,13 @@ opt = simset('ReturnWorkspaceOutputs', 'off');
 sub.reload;
 
 figure
-plot(T,Y(:,1));
+plot(T,Y(:,1).*9.549296596425384);
 title('Wheel Speed')
 xlabel('Time [s]')
-ylabel('Wheel Speed [rad/s]')
+ylabel('Wheel Speed [rpm]')
+
 figure
-plot(Y(:,1),Y(:,2));
-xlabel('Wheel Speed [rad/s]');
+plot(Y(:,1).*9.549296596425384,Y(:,2));
+xlabel('Wheel Speed [rpm]');
 ylabel('Wheel Torque [Nm]');
 title('Lossy chain')
