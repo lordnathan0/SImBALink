c405290ef15d8d61059e285298b1a4fc4108766b
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2014-11-20 14:42:39 -0500

    add test_multiple_fits.m, which tests fit against each of the truth datasets and generates plots

---
+++ Model/Powertrain/Battery pack/Validation/test_multiple_fits.m
@@ -0,0 +1,6 @@
+% Evaluate the model fit against several truth sources.
+
+[sse(1) r2(1)] = test_model_fit('RedCell_1');
+[sse(2) r2(2)] = test_model_fit('RedCell_2');
+[sse(3) r2(3)] = test_model_fit('RedCell_3');
+
