37e12b340dcf4f70e1381394c0028cfeb9008381
Nathan Lord <lord.59@osu.edu>
2014-12-13 13:26:56 -0500

    Validation with Torque Timeseries

---
+++ Model/TRC_val.m
@@ -0,0 +1,62 @@
+%load CoastDownTimeSeries
+load('228hv_tau_coastdown.mat')
+
+datastart = 1300;
+
+close all
+starttime = ts{1}.Data(datastart);
+endtime = 832;
+thr = ts{26};
+rpm = ts{34};
+eff_tyre = -0.01843; %(m/s)/RPM
+
+v0 = rpm.Data(datastart) * eff_tyre;
+
+sample_tau = getsampleusingtime(sim_tau,starttime,endtime);
+sample_tau.Time = sample_tau.Time - sample_tau.Time(1);
+
+rpm_sample = getsampleusingtime(rpm,starttime,endtime);
+rpm_sample.Time = rpm_sample.Time - rpm_sample.Time(1);
+
+
+chas = get_param('Chassis','ModelWorkspace');
+gear = get_param('Gear_Chain','ModelWorkspace');
+tire = get_param('Tires','ModelWorkspace');
+
+assignin(chas,'v0',v0);
+g1 = getVariable(tire,'Ct');
+%gearratio = 1/((-eff_tyre)*(1/g1)*9.54929659643);
+gearratio = 3.1;
+assignin(gear,'w0',(v0)/g1);
+assignin(gear,'Rg',gearratio);
+assignin(chas,'CdA',.4);
+
+rho = 1.2;
+Or = 0;
+Beta = 0;
+V_cmd = 40;
+Rc = 10000;
+
+
+sim('TRC',600);
+
+figure
+hold all
+plot(t,v)
+plot(rpm_sample.Time,rpm_sample.Data*eff_tyre);
+title('Velocity m/s');
+legend('sim','data')
+
+figure
+hold all
+plotyy(t,Or, t, Rc);
+title('Road grad');
+
+
+figure
+plot(t,Tm)
+title('torque');
+
+figure
+plot(t,ks)
+title('slip');
