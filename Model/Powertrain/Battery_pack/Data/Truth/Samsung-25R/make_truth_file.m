e475f5612e10b5709ac94ea2db0ce3969b33a631
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2015-01-30 16:32:30 -0500

    Add 'Samsung-25R' data set

---
+++ Model/Powertrain/Battery_pack/Data/Truth/Samsung-25R/make_truth_file.m
@@ -0,0 +1,16 @@
+% Make the validation truth file
+% abk - Jan 2015
+
+% load file
+input	= 'Curr_Cell4B_PP_110614 - 008.csv';
+[i, v]	= read_maccor(input);
+output	= 'PikesPeak_discharge'
+info	= [ 'Generated ' date ' from ' input ];
+
+% truncate
+start	= 5.849e4;	% sample number, not time
+Idc = delsample(i, 'Index', 1:start );
+V = delsample(v, 'Index', 1:start );
+
+% save
+save( output, 'Idc', 'V', 'info' );
