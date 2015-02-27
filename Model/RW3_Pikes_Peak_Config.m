a1c7417d4b80e6cb242b61842f7056898da0d7dd
Nathan Lord <lord.59@osu.edu>
2015-02-27 14:09:30 -0500

    Created script to reset model defaults

--- Model/RW3_Pikes_Peak_Config.m
+++ Model/RW3_Pikes_Peak_Config.m
@@ -1,4 +1,4 @@
-function [Rider,Env,Battery,MotorController,Motor,Chassis,Gear,Brakes,Tires] = RW3_Pikes_Peak_Config()
+function [Rider,Env,Battery,MotorController,Motor,Chassis,Gear,Brakes,Tires, FileName] = RW3_Pikes_Peak_Config()
 %% add paths
 addpath('Rider');
 addpath('Environment');
@@ -30,43 +30,52 @@ load_system('Tires');
 Tires = get_param('Tires', 'modelworkspace');
 %% Load Correct Config
 %run('Powertrain/Battery_pack/Data/Raw/RW3_generate_parameters.m')
+FileName.Battery = Battery.FileName;
 Battery.FileName = 'Powertrain/Battery_pack/Data/Battery pack.mat' ;
 Battery.reload();		% reload workspace from source file
 
 run('Vehicle/Brakes/Data/RW3_generate_parameters.m')
+FileName.Brakes = Brakes.FileName;
 Brakes.FileName = 'Vehicle/Brakes/Data/RW3_Brakes.mat' ;
 Brakes.reload();		% reload workspace from source file
 
 run('Vehicle/Chassis/Data/RW3_generate_parameters.m')
+FileName.Chassis = Chassis.FileName;
 Chassis.FileName = 'Vehicle/Chassis/Data/RW3_Chassis.mat' ;
 Chassis.reload();		% reload workspace from source file
 
 
 run('Environment/Data/Pikes_Peak_generate_parameters.m')
+FileName.Env = Env.FileName;
 Env.FileName = 'Environment/Data/Pikes_Peak_Environment.mat' ;
 Env.reload();		% reload workspace from source file
 
 %needs to be corrected
 run('Vehicle/Gear_Chain/Data/RW3_generate_parameters.m')
+FileName.Gear = Gear.FileName;
 Gear.FileName = 'Vehicle/Gear_Chain/Data/RW3_Gear.mat' ;
 Gear.reload();		% reload workspace from source file
 
 %needs to be corrected
 %run('Powertrain/Motor/Data/RW3_generate_parameters.m')
+FileName.Motor = Motor.FileName;
 %Motor.FileName = 'Powertrain/Motor/Data/RW3_Motor.mat' ;
 Motor.reload();		% reload workspace from source file
 
 %needs to be corrected
 %run('Powertrain/Motor_controller/Data/RW3_generate_parameters.m')
+FileName.MotorController = MotorController.FileName;
 %MotorController.FileName = 'Powertrain/Motor_controller/Data/RW3_MotorController.mat' ;
 MotorController.reload();		% reload workspace from source file
 
 %needs to be corrected
 run('Rider/Pikes_Peak_generate_parameters.m')
+FileName.Rider = Rider.FileName;
 Rider.FileName = 'Rider/Pikes_Peak_Rider_PI.mat' ;
 Rider.reload();		% reload workspace from source file
 
 run('Vehicle/Tires/Data/RW3_generate_parameters.m')
+FileName.Tires = Tires.FileName;
 Tires.FileName = 'Vehicle/Tires/Data/RW3_Tires.mat' ;
 Tires.reload();		% reload workspace from source file
 end
--- Model/RW3_Pikes_Peak_Run.m
+++ Model/RW3_Pikes_Peak_Run.m
@@ -1,7 +1,7 @@
 %% Run Pikes Peak Simulation with RW3
 
 %% Initialize default config
-[Rider,Env,Battery,MotorController,Motor,Chassis,Gear,Brakes,Tires] = RW3_Pikes_Peak_Config();
+[Rider,Env,Battery,MotorController,Motor,Chassis,Gear,Brakes,Tires, FileNames] = RW3_Pikes_Peak_Config();
 
 %% Make Changes to the Config
 % other values can be changed by looking in the documentation
@@ -18,10 +18,12 @@ assignin(PP,'D_max',1); %set max distance
 
 sim('Pikes_Peak',time);
 
+RW3_Pikes_Peak_Defaults(Rider,Env,Battery,MotorController,Motor,Chassis,Gear,Brakes,Tires, FileNames);
+
 Pikes_Peak_Data = Dataset2Ts(logsout);
 
 %%
 figure
-plot(Pikes_Peak_Data.Velocity);
+plot(Pikes_Peak_Data.Vehicle_Velocity);
 figure
 plot(Pikes_Peak_Data.Wheel_Slip);
